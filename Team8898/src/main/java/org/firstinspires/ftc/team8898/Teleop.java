c38a442a1ea23ce507327bdbc2e37779710112e3
Shane Matsushima <skm1901@iolani.org>
2019-09-09 16:34:53 -1000

    Finished bot and teleop

---
+++ Team8898/src/main/java/org/firstinspires/ftc/team8898/Teleop.java
@@ -0,0 +1,25 @@
+package org.firstinspires.ftc.team8898;
+
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.DcMotor;
+
+@TeleOp(name="Basic: OpMode", group="Iterative Opmode")
+
+public class Teleop extends LinearOpMode{
+    private Bot robot = new Bot(this);
+
+    double frontLeftPower;
+    double frontRightPower;
+    double backLeftPower;
+    double backRightPower;
+
+    @Override
+    public void runOpMode() throws InterruptedException {
+       robot.init(hardwareMap);
+
+       robot.setPower(gamepad1.left_stick_y, gamepad1.right_stick_y);
+    }
+
+}
