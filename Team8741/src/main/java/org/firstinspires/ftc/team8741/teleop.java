e78aa0280ac33f52bbae26cb7c57c4672e534a7e
Jack Gonser <TheKiller65YT@users.noreply.github.com>
2019-09-09 17:21:00 -1000

    added teleop

---
+++ Team8741/src/main/java/org/firstinspires/ftc/team8741/teleop.java
@@ -0,0 +1,12 @@
+package org.firstinspires.ftc.team8741;
+
+public class teleop {
+    private static final teleop ourInstance = new teleop();
+
+    public static teleop getInstance() {
+        return ourInstance;
+    }
+
+    private teleop() {
+    }
+}
