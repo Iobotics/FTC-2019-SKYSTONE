4c03a7fd5cd9764024f5720dd6831a4eebc281c2
Jack Gonser <TheKiller65YT@users.noreply.github.com>
2019-09-11 17:28:06 -1000

    added testing code for servos

--- Team8741/src/main/java/org/firstinspires/ftc/team8741/teleop.java
+++ Team8741/src/main/java/org/firstinspires/ftc/team8741/teleop.java
@@ -1,12 +1,43 @@
 package org.firstinspires.ftc.team8741;
 
-public class teleop {
-    private static final teleop ourInstance = new teleop();
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorSimple;
 
-    public static teleop getInstance() {
-        return ourInstance;
+import org.firstinspires.ftc.robotcore.external.Telemetry;
+
+@TeleOp(name="Basic: OpMode", group="Iterative Opmode")
+
+public class teleop extends LinearOpMode {
+    private Bot robot = new Bot(this);
+
+    double frontLeftPower;
+    double frontRightPower;
+    double backLeftPower;
+    double backRightPower;
+
+
+
+    @Override
+    public void runOpMode() throws InterruptedException {
+        robot.init(hardwareMap);
+        waitForStart();
+        while (opModeIsActive()) {
+            robot.setPower(gamepad1.left_stick_y, gamepad1.right_stick_y);
+
+
+            robot.setLifter(gamepad1.a, gamepad1.b);
+            telemetry.addData("Lifter 1", robot.getLifter1Servo());
+
+            telemetry.update();
+
+            robot.setCloser(gamepad1.y, gamepad1.x);
+
+
+        }
     }
 
-    private teleop() {
-    }
+
 }
