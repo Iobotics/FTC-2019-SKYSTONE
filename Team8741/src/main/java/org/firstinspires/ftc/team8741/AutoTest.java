cd68bd162b72e7b2e9b6d1e9ffa8799efe017c59
Jack Gonser <TheKiller65YT@users.noreply.github.com>
2019-09-30 17:29:38 -1000

    added auto2. thank you Jack.

---
+++ Team8741/src/main/java/org/firstinspires/ftc/team8741/AutoTest.java
@@ -0,0 +1,19 @@
+package org.firstinspires.ftc.team8741;
+
+
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.util.ElapsedTime;
+
+
+@com.qualcomm.robotcore.eventloop.opmode.Autonomous(name="Autonomous", group = "Bot")
+//@Disabled
+public class AutoTest extends LinearOpMode {
+
+    private Bot robot = new Bot(this);
+    private ElapsedTime runtime = new ElapsedTime();
+
+    @Override
+    public void runOpMode() throws InterruptedException {
+        robot.gyroTurn(90, 0.5);
+    }
+}
--- Team8898/src/main/java/org/firstinspires/ftc/team8898/Auto.java
+++ Team8898/src/main/java/org/firstinspires/ftc/team8898/Auto.java
@@ -11,10 +11,8 @@ public class Auto extends LinearOpMode {
         robot.init(hardwareMap);
         robot.resetEncoder();
         waitForStart();
-<<<<<<< HEAD
         robot.encoderDrive(1, -10, -10,60);
 
-=======
         robot.encoderDrive(.8, 20, 20,30);
         robot.gyroTurn(-90,.5);
         robot.encoderDrive(.8,63,63,30);
@@ -34,6 +32,5 @@ public class Auto extends LinearOpMode {
         while(opModeIsActive()) {
             telemetry.update();
         }
->>>>>>> 30a6104716ea719aa5d789ea1c71d8e7b894829c
     }
 }
