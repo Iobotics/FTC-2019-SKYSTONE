d6fb54817f364cb1687e5a23cf8b22b5611dbcd9
Mallhew <mah2001@iolani.org>
2019-10-21 17:25:44 -1000

    finished teleop, confirmed auto

--- Team8740/src/main/java/org/firstinspires/ftc/team8740/Auto.java
+++ Team8740/src/main/java/org/firstinspires/ftc/team8740/Auto.java
@@ -11,15 +11,10 @@ import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
             robot.init(hardwareMap);
             waitForStart();
             while (opModeIsActive()) {
-            robot.encoderDrive(0.1,-5,-5,5);
+            robot.encoderDrive(0.9,-10,-10,5);
             robot.encoderTurn(0.1,-45,5);
             robot.encoderDrive(0.1,-1,-1,5);
+
             }
         }
         }
-<<<<<<< HEAD
-
-yeet
-
-=======
->>>>>>> 0d372993ee4c77a620c72c517516e44764ed58ec
